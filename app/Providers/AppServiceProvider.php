0f18f4158fd1c8e2176f20dda293d3e0aa048c65
evanhelgas <evanabaddon@gmail.com>
2024-12-01 20:39:35 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -62,6 +62,10 @@ class AppServiceProvider extends ServiceProvider
             URL::forceScheme('https');
         }
 
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
             $switch
                 ->locales(['id','en'])->circular(); // also accepts a closure
